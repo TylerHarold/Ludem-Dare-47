9b777ba009b5e523dc8b464db9078c17df731d55
Tyler <tyler@programmer.net>
2020-10-02 20:48:21 -0400

    Input structure (WTF IS HAPPENING WITH GITIGNORE??)

---
+++ LD 47/Assets/Tyler/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private GameState state;
+
+    private UIManager UI;
+    private GameInput game;
+    private Dialogue dialogue;
+    private Pause pause;
+
+
+    public enum GameState
+    {
+        UI,
+        GAME,
+        DIALOGUE,
+        PAUSE
+    }
+
+    private void Update()
+    {
+        switch(state)
+        {
+            case GameState.UI:
+                // UIManager.Initiate();
+                break;
+            case GameState.GAME:
+                // Game.Initiate();
+                break;
+            case GameState.DIALOGUE:
+                // Dialogue.Initiate();
+                break;
+            case GameState.PAUSE:
+                // Pause.Initiate();
+                break;
+        }
+    }
+
+
+}
---
+++ LD 47/Assets/Tyler/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Menu
+    private Transform menu_ButtonsParent;
+    private Button menu_PlayButton;
+    private Button menu_OptionsButton;
+    private Button menu_QuitButton;
+
+
+
+
+    public IEnumerator TakeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        yield return new WaitForSeconds(1f);
+    }
+
+    public IEnumerator MenuLoad()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+}
